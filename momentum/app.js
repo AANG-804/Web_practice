8b4a1b4eb31ea3c9f3fed1a0b4e85b66066ca363
Ahn Jaehyun <bonjour8084@pusan.ac.kr>
2024-03-30 23:45:48 +0900

    Studied Event listner

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,30 @@
-console.dir(document)
+function handleTitleClick() {
+    console.log("text has been clicked")
+}
+
+function handleMouseEnter(){
+    text.style.color = "blue"
+    text.innerText = "Mouse is IN!"
+    console.log("Mouse Enter~")
+}
+
+function handleMouseLeave(){
+    text.style.color = "red"
+    text.innerText = "Mouse is Out!"
+    console.log("Mouse leaves~")
+}
+
+function handleResize() {
+    document.body.style.backgroundColor = "tomato"
+}
+
+const text = document.querySelector('div.titles:first-child p');
+text.addEventListener("click", handleTitleClick);
+text.addEventListener("mouseenter", handleMouseEnter);
+text.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleResize);
+
+
+
+
