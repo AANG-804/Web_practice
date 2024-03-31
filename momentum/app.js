299701471deaa4632e1e7829f815789fed6f1d3c
Ahn Jaehyun <bonjour8084@pusan.ac.kr>
2024-03-31 14:27:28 +0900

    Login Element-Done

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,41 @@
-function handleTitleClick() {
-    const clickedClassName = "active"
-    // if (h1.classList.contains(clickedClassName)) {
-    //     h1.classList.remove(clickedClassName)
-    // } else {
-    //     h1.classList.add(clickedClassName)
-    // }
-    h1.classList.toggle(clickedClassName);
+const logInForm = document.querySelector("#login-form");
+const logInInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASS_NAME = "hidden"
+const USER_NAME_KEY = "userName"
+
+function onLogInSubmit(event){
+    // 로그인 창 숨기기
+    event.preventDefault();
+    console.log(logInInput.value);
+    logInForm.classList.add(HIDDEN_CLASS_NAME);
+
+    // 인사 전하기
+    const userName = logInInput.value;
+    paintGreetings(userName);
+
+    // 유저 이름 저장하기
+    window.localStorage.setItem(USER_NAME_KEY, userName);
+
 }
 
-const h1 = document.querySelector('div.titles:first-child h1');
+function paintGreetings(username){
+    greeting.innerText = `Hi there, nice to meet you ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS_NAME);
+}
 
-h1.addEventListener("click", handleTitleClick);
 
+const savedUserName = window.localStorage.getItem(USER_NAME_KEY);
+
+if (savedUserName === null) {
+    // show form
+    logInForm.classList.remove(HIDDEN_CLASS_NAME);
+    logInForm.addEventListener('submit', onLogInSubmit);
+} else {
+    // show greeting
+    paintGreetings(savedUserName);
+}
 
 
 
