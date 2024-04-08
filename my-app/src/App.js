8b29ed7c0d29d03ce90133a4b33ff0d1a2675cba
Jaehyun Ahn <bonjour8084@naver.com>
2024-04-08 12:34:10 +0900

    Movie App dev Class-Effect Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,19 +4,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
 
+  function onSearch(event) {
+    return setKeyword(event.target.value);
+  }
   function onClick() {
     return setValue((prev) => prev+1)
   }
-  console.log("I run all the time")
-  const iRunOnlyOnce = () => {
-    console.log("I run only ONCE!");
+
+  function searchAPI(keyword) {
+    console.log("Search For", keyword);
   }
-  useEffect(iRunOnlyOnce, []);
+  
+  useEffect(() => {
+    if ((keyword !== "") && (keyword.length > 5)){
+      searchAPI(keyword);
+    }
+  }, [keyword]);
+  
+
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
       <Button text="Click Me" onClick={onClick}></Button>
+      <input value={keyword} onChange={onSearch} type="text" placeholder="Search Here!"/>
     </div>
   );
 }
