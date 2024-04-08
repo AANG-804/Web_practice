52f6eb52f7b3e1acb3bf624a077172107d3e7ad2
Jaehyun Ahn <bonjour8084@naver.com>
2024-04-08 15:25:43 +0900

    todo list Practice Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,43 @@ import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
 function App() {
-  const [hide, setHide] = useState(false);
 
-  const onClick = () => {
-    setHide(prev => !prev);
-  };
-  
-  function Hello() {
-    useEffect(() => {
-      console.log("I'm Here!");
-      return () => console.log('Bye');
-    }, []);
-    return <h1>Hello!</h1>
+  const [todo, setTodo] = useState("");
+  const onChange = (event) => {setTodo(event.target.value)};
+  const [todos, setTodos] = useState([]);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos(currentArray => [todo, ...currentArray]);
+    setTodo("");
   }
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      console.log(todos)
+    };
+  }, [todos]);
+
+  
   return (
     <div>
-      <button onClick={onClick}>{hide ? "Show" : "Hide"}</button>
-      {hide ? null : <Hello />}
+      <h1>My todos! ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={todo} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="Write your to do..." 
+        />
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+      {todos.map((el, idx) => <li key={idx}>{el}</li>)}
+      </ul>
+      
     </div>
   );
 }
